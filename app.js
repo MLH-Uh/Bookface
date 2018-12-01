728fa14232ce53aff10ba88c0a937ccd7e216920
TeamDman <TeamDman9201@gmail.com>
2018-12-01 13:51:23 -0500

    Moved message to its own file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const token = require('./token.json');
+const fs = require('fs');
 const path = require('path');
 const login = require('facebook-chat-api');
-// login({email: token.email, password: token.password}, (err, api) => {
-// 	if (err) return console.error(err);
-// 	api.listen((err, message) => {
-//         api.sendMessage('suh dude', message.threadID);
-//     });
-// });
+const express = require('express')
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const token = require('./token.json');
+const port = 3000
+
+
 
 let messages = [
 	{name: "Oof", content: "woot", avatar:"https://i.imgur.com/VWcKTrh.jpg"},
@@ -14,10 +16,26 @@ let messages = [
 	{name: "Oof", content: "doot", avatar:"http://www.veggieonapenny.com/wp-content/uploads/2013/11/vegan-cheese-3-1024x731.jpg"}
 ];
 
-const express = require('express')
-const port = 3000
-express()
-	.use(express.static(path.join(__dirname, 'public')))
+let creds;
+if (fs.exists("appstate.json")) { 
+	creds = require('./appstate.json');
+} else {
+	creds = {email: token.email, password: token.password};
+}
+login(creds, (err, api) => {
+	if (err) return console.error(err);
+	api.listen((err, message) => {
+		console.log(message.threadID);
+        api.sendMessage('suh dude', message.threadID);
+    });
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+	// setInterval(()=>{
+			api.getThreadHistory("100010346405871",5, undefined, (err, hist) => {
+				messages.push({name:"Dom", content: hist[0].body, avatar: "https://i.imgur.com/JsqkGWc.jpg"});
+			});
+	// },3000);
+});
+app	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('index', {messages: messages}))
@@ -34,4 +52,12 @@ express()
 	//   });
 	//   res.render('pages/search', {search: search, query: req.query.query});
 	// })
-	.listen(port, () => console.log(`Listening on ${port}`));
+
+
+io.on('connection', client => {
+	console.log('Client connected...');
+	client.on('join', data => {
+		console.log(data);
+	});
+});
+server.listen(port, () => console.log(`Listening on ${port}`));
