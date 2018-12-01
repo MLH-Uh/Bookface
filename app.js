7f8d759ca688e0adbb0f5278e655f2f2fa15d2b9
TeamDman <TeamDman9201@gmail.com>
2018-12-01 11:18:32 -0500

    Created basic message display

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const token = require("./token.json")
-const login = require("facebook-chat-api");
-login({email: token.email, password: token.password}, (err, api) => {
-	if (err) return console.error(err);
-	api.listen((err, message) => {
-        api.sendMessage("suh dude", message.threadID);
-    });
-});
+const token = require('./token.json');
+const path = require('path');
+const login = require('facebook-chat-api');
+// login({email: token.email, password: token.password}, (err, api) => {
+// 	if (err) return console.error(err);
+// 	api.listen((err, message) => {
+//         api.sendMessage('suh dude', message.threadID);
+//     });
+// });
+
+let messages = [
+	{name: "Oof", content: "woot", avatar:"https://i.imgur.com/VWcKTrh.jpg"},
+	{name: "Oof", content: "boot", avatar:"https://i.imgur.com/VWcKTrh.jpg"},
+	{name: "Oof", content: "doot", avatar:"https://i.imgur.com/VWcKTrh.jpg"}
+];
 
 const express = require('express')
-const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+express()
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('index', {messages: messages}))
+	// .get('/search', async (req, res) => {
+	//   console.log(`Searching for '${req.query.query}'`);
+	//   let search = await client.search({
+	//     index: 'serviceproviders',
+	//     type: 'provider',
+	//     body: {
+	//       query: {
+	//         regexp: {'desc': req.query.query || ''}
+	//       }
+	//     }
+	//   });
+	//   res.render('pages/search', {search: search, query: req.query.query});
+	// })
+	.listen(port, () => console.log(`Listening on ${port}`));
